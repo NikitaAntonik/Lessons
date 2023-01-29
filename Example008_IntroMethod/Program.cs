cde12d74029521de88b302fcf759dfad086c4c10
Antonik Nikita <n_antonik@mail.ru>
2023-01-29 19:18:09 +0300

    Методы функции

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 15;
+int b2 = 211;
+int c2 = 31;
+int a3 = 17;
+int b3 = 22;
+int c3 = 30;
+
+int max = Max(Max (a1, b1, c1), Max (a2, b2, c2), Max (a3, b3, c3));
+Console.WriteLine(max);
