6968c8ddfdc0e8c81de5777d857a7fcf2adf6a0e
Antonik Nikita <n_antonik@mail.ru>
2023-01-29 19:33:36 +0300

    array

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -5,8 +5,12 @@
     if (arg3 > result) result = arg3;
     return result;
 }
+int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 29};
 
+int result = Max(
+    Max (array[0], array[1], array[2]), 
+    Max (array[3], array[4], array[5]), 
+    Max (array[6], array[7], array[8]));
 
-int max = Max(Max (a1, b1, c1), Max (a2, b2, c2), Max (a3, b3, c3));
-Console.WriteLine(max);
+Console.WriteLine(result);
 
